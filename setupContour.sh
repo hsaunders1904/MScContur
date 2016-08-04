afc93cf06206e606cc21cc046937976e7ad6d04e
dyallup <devnull@localhost>
2016-08-04 10:01:09 +0000

    minor fixes

--- setupContour.sh
+++ setupContour.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-##edit this to location of svn repo on your system
 export CONTURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export RIVET_DATA_PATH=$CONTURDIR/modified_analyses/refdata
