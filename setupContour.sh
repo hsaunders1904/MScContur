83369f4881a51b201599ff471c305404458f64cd
dyallup <devnull@localhost>
2016-08-08 14:21:42 +0000

    changing the plotter

--- setupContour.sh
+++ setupContour.sh
@@ -4,6 +4,8 @@ export CONTURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export RIVET_DATA_PATH=$CONTURDIR/modified_analyses/refdata
 export RIVET_ANALYSIS_PATH=$CONTURDIR/modified_analyses/Analyses
+export RIVET_PLOT_PATH=$CONTURDIR/modified_analyses/plotinfo
+export RIVET_REF_PATH=$CONTURDIR/modified_analyses/anainfo
 
 export PYTHONPATH=${PYTHONPATH}:$CONTURDIR/AnalysisTools/contour
 export PATH=${PATH}:$CONTURDIR/AnalysisTools/contour/bin/
