274dfd3e7e471c6ae12f75dd39c48fa48727a94f
sarrazin <devnull@localhost>
2016-08-12 08:46:59 +0000

    Copied ATLAS_2013_I1230812_EL/MU.info from Rivet-2.5.1 to ATLAS_2013_I1230812_HF_EL/MU.info. Appended _HF to all modified analyses names. Set the RIVET_INFO_PATH env variable.

--- setupContour.sh
+++ setupContour.sh
@@ -5,7 +5,7 @@ export CONTURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export RIVET_DATA_PATH=$CONTURDIR/modified_analyses/refdata
 export RIVET_ANALYSIS_PATH=$CONTURDIR/modified_analyses/Analyses
 export RIVET_PLOT_PATH=$CONTURDIR/modified_analyses/plotinfo
-export RIVET_REF_PATH=$CONTURDIR/modified_analyses/anainfo
+export RIVET_INFO_PATH=$CONTURDIR/modified_analyses/anainfo
 
 export PYTHONPATH=${PYTHONPATH}:$CONTURDIR/AnalysisTools/contour
 export PATH=${PATH}:$CONTURDIR/AnalysisTools/contour/bin/
