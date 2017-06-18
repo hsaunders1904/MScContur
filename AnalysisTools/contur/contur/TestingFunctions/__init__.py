62c44caf201255ca8a45145c5d18213f9879d87b
dgrell <devnull@localhost>
2017-06-18 11:23:57 +0000

    Make old data default again. Use CONTURTEST=new environment variable to switch.

--- AnalysisTools/contur/contur/TestingFunctions/__init__.py
+++ AnalysisTools/contur/contur/TestingFunctions/__init__.py
@@ -1,4 +1,13 @@
 from .TesterFunctions import *
-# from .StaticData import *
-from .StaticDB import *
-init_dbs()
+
+import os
+
+if os.getenv('CONTURTEST') != 'new':
+	print "Using hand-coded data"
+	from .StaticData import *
+else:
+	print "Using DB"
+	from .StaticDB import *
+	init_dbs()
+
+del os
