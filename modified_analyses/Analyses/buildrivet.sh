47f57f8df80ec3530bc65b295cabda50aea7c69b
jmb <devnull@localhost>
2017-07-05 09:02:58 +0000

    remove buggy modified routine. rivet relese now fixed.

--- modified_analyses/Analyses/buildrivet.sh
+++ modified_analyses/Analyses/buildrivet.sh
@@ -2,6 +2,8 @@
 
 # Execute to build all Rivet routines in this directory                         
 
+rm *.so
+
 for routine in *.cc; do
     rivet-buildplugin Rivet${routine%.cc}.so --std=c++11 $routine
 done
